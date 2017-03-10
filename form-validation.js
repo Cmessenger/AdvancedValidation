ba709be487547bb686e10883b62990069b759a28
Cmessenger <cmessenger@mail.csuchico.edu>
2017-03-09 22:43:30 -0800

    Add files via upload

--- form-validation.js
+++ form-validation.js
@@ -2,18 +2,24 @@ function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
-
+  var = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var sln = txt.length;
   //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
+  document.getElementById("hello").innerHTML="Bad username.";
+  document.getElementById("hello").classList.remove("password");
+  <input type="password" id="chicostate" value="psw123">
+  <p>Click the button to get the password of the password field.</p>
+  <button onclick="myFunction()">Try it</button>
+  <p id="password"></p>
+  document.getElementById("hello").classList.add("shown-message");
+  document.getElementById("username").innerHTML = Claremessenger
   //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
-
+  document.getElementById("usernameGroup").classList.add("has-success");
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+  document.getElementById("passwordGroup").classList.add("has-success");
 }
+Contact GitHub API Training Shop Blog About
